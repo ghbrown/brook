8751c064d7d2e5898d900662af8d8230fa9c91a2
ghbrown <gabriel.h.brown@gmail.com>
2022-02-08 23:38:45 -0600

    README and plan

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
-### pypack
+## brook*
 
-A scaffolding for (my way) of developing a Python package
+Compute finite difference approximations to derivatives of multidimensional, multivariate functions.
+
+Unlike [`findiff`](https://github.com/maroba/findiff) and [`fastfd`](https://github.com/stefanmeili/FastFD), the focus of `brook` is on computing higher order derivative objects (gradients, Jacobians, Hessians, etc.) for problems like optimization and the solution of nonlinear equations.
+Of course, it may also be used to numerically verify handcoded implementations of said objects.
+
+\* after [Brook Taylor](https://en.wikipedia.org/wiki/Brook_Taylor), the namesake for Taylor series and the originator of finite differences
+
+
+## Planned workflow for repo
+See `media/plan.jpg` for the proposed workflow to generate the desired derivatives
--- setup.py
+++ setup.py
@@ -2,35 +2,36 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name=<PACKAGE_NAME>,  
+    name=brook,  
     version='0.0.0',
-    scripts=['bin/'<PACKAGE_NAME>] ,
-    author="Gabriel H. Brown",
-    author_email="gabriel.h.brown@gmail.com",
-    description=<PACKAGE_DESCRIPTION>,
+    scripts=['bin/brook'] ,
+    author='Gabriel H. Brown',
+    author_email='gabriel.h.brown@gmail.com',
+    description='Finite differences for multidimensional, multivariate functions',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ghbrown/"<PACKAGE_NAME>,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ghbrown/brook',
     packages=setuptools.find_packages(),
     #include_package_data = True, #include non-Python files specified in MANIFEST.in
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
     ],
     keywords=[
-        <LIST_OF_KEYWORDS>
-        "scientific",
-        "engineering",
+        'scientific',
+        'engineering',
+        'derivatives',
+        'optimization',
+        'rootfinding',
+        'approximation',
     ],
     install_requires=[
-        <REQUIRED_PACKAGES>
-        "numpy",
-        "matplotlib",
+        'numpy',
     ],
  )
